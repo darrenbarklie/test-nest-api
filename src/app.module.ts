51b8d441ac103df0ede882f49d54accdf2a3ad4a
Darren Barklie <darrenbarklie@gmail.com>
2022-04-13 19:43:22 +0100

    Remove database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 // import { ConfigModule } from '@nestjs/config';
 // import * as Joi from '@hapi/joi';
 
@@ -9,23 +9,23 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        // host: '127.0.0.1',
-        host: '10.77.80.3',
-        // port: 1234,
-        port: 5432,
-        username: 'postgres',
-        password: '5APs4Edojig6fml7',
-        database: 'postgres',
-        autoLoadEntities: true,
-        synchronize: true,
-        // IMPORTANT: disable synchronize setting in Production
-        // Automatically generates a SQL table from all classes
-        // with an @Entity decorator and the metadata contained
-      }),
-    }),
+    // TypeOrmModule.forRootAsync({
+    //   useFactory: () => ({
+    //     type: 'postgres',
+    //     host: '127.0.0.1',
+    //     // host: '10.77.80.3',
+    //     port: 1234,
+    //     // port: 5432,
+    //     username: 'postgres',
+    //     password: '5APs4Edojig6fml7',
+    //     database: 'postgres',
+    //     autoLoadEntities: true,
+    //     synchronize: true,
+    //     // IMPORTANT: disable synchronize setting in Production
+    //     // Automatically generates a SQL table from all classes
+    //     // with an @Entity decorator and the metadata contained
+    //   }),
+    // }),
     // ConfigModule.forRoot({
     //   envFilePath: ['.env'],
     //   validationSchema: Joi.object({
